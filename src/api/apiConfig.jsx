80254c8376791532cc1d77b9a39871b0d2054abe
Kira <thiennguyen.fsd@gmail.com>
2021-12-31 09:49:01 +0700

    delete api key

--- src/api/apiConfig.jsx
+++ src/api/apiConfig.jsx
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.openweathermap.org/data/2.5',
-    apiKey: '068eee1a0c4e028c65a7a727e098a332',
+    apiKey: 'Your api key',
     iconImage: (imgPath) => `http://openweathermap.org/img/wn/${imgPath}@2x.png`
 }
 
